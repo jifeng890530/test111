4baea0aac9883eab64659ea3903ea9a20173290c
jifeng890530 <qq505420957@gmail.com>
2025-04-04 11:03:10 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,16 @@
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
-
 import type { NextRequest } from 'next/server'
-import { Database } from './types/supabase'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
+  const supabase = createMiddlewareClient({ req, res })
   await supabase.auth.getSession()
   return res
+}
+
+export const config = {
+  matcher: [
+    '/((?!api|_next/static|_next/image|images|favicon.ico).*)',
+  ],
 }
